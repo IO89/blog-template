f3bb6f5f74e83ebf0716b7a2a23ad076ed87abd6
io89 <ivan.osipov@futurice.com>
2020-09-26 18:55:58 +0300

    Remove navigation

--- src/templates/blog-post-contentful.js
+++ src/templates/blog-post-contentful.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import Bio from "../components/bio";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const BlogPostContentfulTemplate = ({ data, pageContext, location }) => {
+const BlogPostContentfulTemplate = ({ data, location }) => {
   const post = data.contentfulPost;
   const siteTitle = data.site.siteMetadata?.title || `Title`;
-  const { previous, next } = pageContext;
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -37,32 +36,6 @@ const BlogPostContentfulTemplate = ({ data, pageContext, location }) => {
         <hr />
         <footer />
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.slug} rel="prev">
-                ← {previous.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.slug} rel="next">
-                {next.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
     </Layout>
   );
 };
